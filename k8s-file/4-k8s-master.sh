4bcfc877fa6b266220136a6ad33363d506e6e194
Ruby-Adhana <145450062+Ruby-Adhana@users.noreply.github.com>
2024-07-06 13:24:55 +0530

    Update 4-k8s-master.sh

--- k8s-file/4-k8s-master.sh
+++ k8s-file/4-k8s-master.sh
@@ -2,3 +2,17 @@ sudo kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-advertise-addres
 mkdir -p $HOME/.kube 
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config 
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+sudo kubeadm init --pod-network-cidr=192.168.0.0/16 --apiserver-cert-extra-sans=kubeadmclstr.esatasia.cloudapp.azure.com --control-plane-endpoint=104.214.186.103
+
+
+You can now join any number of control-plane nodes by copying certificate authorities
+and service account keys on each node and then running the following as root:
+
+  kubeadm join 104.214.186.103:6443 --token wt03qy.luhl3346z8xrexb8 \
+        --discovery-token-ca-cert-hash sha256:8a9320bf21c07f9c46c30590b60c524ab9b04c319d66859c107a89f383a5e105 \
+        --control-plane 
+
+Then you can join any number of worker nodes by running the following on each as root:
+
+kubeadm join 104.214.186.103:6443 --token wt03qy.luhl3346z8xrexb8 \
+        --discovery-token-ca-cert-hash sha256:8a9320bf21c07f9c46c30590b60c524ab9b04c319d66859c107a89f383a5e105 
